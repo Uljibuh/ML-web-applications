3abc6c448798b6d3744a426cd8a67013a614b873
Uljibuh Borjigin <75124556+Uljibuh@users.noreply.github.com>
2022-03-13 16:03:13 +0100

    Add files via upload

---
+++ MLCL-streamlit-demo/main.py
@@ -0,0 +1,113 @@
+import streamlit as st
+import numpy as np 
+from sklearn import datasets
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+
+from sklearn.model_selection import train_test_split 
+from sklearn.metrics import accuracy_score
+from sklearn.decomposition import PCA
+
+import matplotlib.pyplot as plt 
+
+
+# set title
+st.title("ML classification demo")
+
+# write text
+st.write("""
+# Explore different machine learning model for classifier
+which model is your favourite?
+""")
+
+# adding select box
+# st.sidebar is for making sidebar
+dataset_name = st.sidebar.selectbox("Select Dataset", ("Iris", "Breast Cancer", "Wine"))
+
+classifier_name = st.sidebar.selectbox("Select Classifier", ("KNN", "SVM", "Radom Forest"))
+
+
+def get_dataset(dataset_name):
+    if dataset_name == "Iris":
+        data = datasets.load_iris()
+    elif dataset_name == "Breast Cancer":
+        data = datasets.load_breast_cancer()
+    else:   
+        data = datasets.load_wine()    
+    X = data.data
+    y = data.target 
+    return X, y
+
+X, y = get_dataset(dataset_name)
+st.write("shape of datasets", X.shape)
+st.write("number of classes", len(np.unique(y)))   
+
+def add_parameter_ui(clf_name):
+    params = dict()
+    if clf_name == "KNN":
+        K = st.sidebar.slider("K", 1, 15)
+        params["K"] = K
+    elif clf_name == "SVM":
+        C = st.sidebar.slider("C", 0.01, 10.0)
+        params["C"] = C
+    else:
+        max_depth = st.sidebar.slider("max_depth", 2, 15)  
+        n_estimators = st.sidebar.slider("n_estimators", 1, 10)
+        params["max_depth"] = max_depth
+        params["n_estimators"] = n_estimators
+    return params    
+
+params = add_parameter_ui(classifier_name)
+
+def get_classifier(clf_name, params):
+    if clf_name == "KNN":
+        clf = KNeighborsClassifier(n_neighbors=params["K"])
+    elif clf_name == "SVM":
+        clf = SVC(C=params["C"])  
+    else:
+        clf = RandomForestClassifier(n_estimators=params["n_estimators"],
+                                     max_depth=params["max_depth"], random_state=1234)
+    return clf
+
+
+clf = get_classifier(classifier_name, params)
+
+
+# classification
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test) 
+
+acc = accuracy_score(y_test, y_pred)
+st.write(f"classifier = {classifier_name}")
+st.write(f"accuracy = {acc}")
+
+# plot
+pca = PCA(3)
+
+X_projected = pca.fit_transform(X)
+
+x1 = X_projected[:, 0]
+x2 = X_projected[:, 1]
+
+fig, a = plt.subplots()
+fig = plt.figure()
+plt.scatter(x1, x2, c=y, alpha=0.5, cmap="viridis")
+plt.xlabel("Principal Component 1")
+plt.ylabel("Principal Component 2")
+plt.colorbar()
+
+
+showPyplotGlobalUse = False
+
+
+st.pyplot(fig)
+
+# TO DO LIST
+# add more parameters
+# add more model
+# add feature scaling
